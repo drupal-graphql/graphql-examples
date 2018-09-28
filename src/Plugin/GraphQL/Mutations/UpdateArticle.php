f91f99b57f0a078289a6e0d987bb7078a1b4b897
Daniel Noyola <daniel@noyola.me>
2018-09-28 09:21:34 -0600

    The current examples are out of date (#7)

--- src/Plugin/GraphQL/Mutations/UpdateArticle.php
+++ src/Plugin/GraphQL/Mutations/UpdateArticle.php
@@ -2,9 +2,10 @@
 
 namespace Drupal\graphql_examples\Plugin\GraphQL\Mutations;
 
-use Drupal\graphql\GraphQL\Type\InputObjectType;
+use Drupal\graphql\Annotation\GraphQLMutation;
+use Drupal\graphql\GraphQL\Execution\ResolveContext;
 use Drupal\graphql_core\Plugin\GraphQL\Mutations\Entity\UpdateEntityBase;
-use Youshido\GraphQL\Execution\ResolveInfo;
+use GraphQL\Type\Definition\ResolveInfo;
 
 /**
  * Simple mutation for updating an existing article node.
@@ -27,7 +28,12 @@ class UpdateArticle extends UpdateEntityBase {
   /**
    * {@inheritdoc}
    */
-  protected function extractEntityInput(array $args, ResolveInfo $info) {
+  protected function extractEntityInput(
+    $value,
+    array $args,
+    ResolveContext $context,
+    ResolveInfo $info
+  ) {
     return array_filter([
       'title' => $args['input']['title'],
       'body' => $args['input']['body'],
